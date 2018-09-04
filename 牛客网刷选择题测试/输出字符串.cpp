bc3d3a1823985029711d6573e050a0108b733ba6
zhaohang <zhaohang19950921@gmail.com>
2018-09-04 18:44:51 +0800

    niu ke

---
+++ 牛客网刷选择题测试/输出字符串.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char str[] = "abcde";
+    cout << str << endl;
+    char *p = "abcde";
+    cout << p << endl;
+    // int array[][4];
+    return 0;
+}
