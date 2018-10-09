6492e76142a5b6ec3d4e16069fd2fd5347cfa3f5
zhaohang <zhaohang19950921@gmail.com>
2018-10-09 14:07:47 +0800

    秋招

---
+++ 2018秋招/1008字节跳动/2018字节跳动笔试编程题1.cpp
@@ -0,0 +1,27 @@
+/*
+
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    vector<int> m(256, -1);
+    int res = 0, left = -1;
+    for (int i = 0; i < s.size(); ++i) {
+        left = max(left, m[s[i]]);
+        m[s[i]] = i;
+        res = max(res, i - left);
+    }
+    return res;
+}
+
+int main() {
+	string str;
+    cin >> str;
+	cout << lengthOfLongestSubstring(str) << endl;
+	return 0;
+}
