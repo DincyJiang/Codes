b8ffee055b894fac5be5a4d519d6c9c21f5838ad
zhaohang <zhaohang19950921@gmail.com>
2018-11-10 15:07:58 +0800

    Update LeetCode/LeetCode_83_删除排序链表中的重复元素.cpp

---
+++ LeetCode/LeetCode_83_删除排序链表中的重复元素.cpp
@@ -0,0 +1,29 @@
+// 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+// 示例 1:
+
+// 输入: 1->1->2
+// 输出: 1->2
+// 示例 2:
+
+// 输入: 1->1->2->3->3
+// 输出: 1->2->3
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return head;
+        ListNode *pre = head;
+        while (pre->next) {
+            if (pre->val == pre->next->val) pre->next = pre->next->next;
+            else pre = pre->next;
+        }
+        return head;
+    }
+};
