6c5bf7b36534e23be0e3979638ebe2d6b587df99
zhaohang <zhaohang19950921@gmail.com>
2018-11-12 14:15:22 +0800

    Update 读书笔记/Cpp并发编程实战/2_4_使用分离线程去处理其他文档.cpp

--- 读书笔记/Cpp并发编程实战/2_4_使用分离线程去处理其他文档.cpp
+++ 读书笔记/Cpp并发编程实战/2_4_使用分离线程去处理其他文档.cpp
@@ -5,8 +5,8 @@ void edit_document(std::string const& filename) {
         user_command cmd = get_user_input();
         if (cmd.type == open_new_document) {
             std::string const new_name = get_filename_from_user();
-            std::thread t(edit_document, new_name);  // 1
-            t.detach();  // 2
+            std::thread t(edit_document, new_name);  // 如果用户选择打开一个新文档，需要启动一个新线程去打开新文档
+            t.detach();  // 并分离线程
         } else {
             process_user_input(cmd);
         }
